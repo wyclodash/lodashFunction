36f084d0c11772b33a6408f2560184e4669fd1ce
unknown <zz860926@egmail.com>
2018-10-12 11:00:21 +0800

    finished flattenDeep()

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ module.exports = {
     difference: require('./lib/difference'),
     drop: require('./lib/drop'),
     flattenDeep: require('./lib/flattendeep'),
-    flattenDepth: require('./lib/flattendepth')
+    flattenDepth: require('./lib/flattendepth'),
+    tail: require('./lib/tail'),
+    join: require('./lib/join'),
+    reverse: require('./lib/reverse')
 }
